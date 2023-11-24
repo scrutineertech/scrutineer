a31bb33396a9a0d4277797dad99212a29b1c9912
Raphael Sprenger <6730690+raphting@users.noreply.github.com>
2023-11-24 18:49:44 +0100

    remove unused function

--- internal/util/util.go
+++ internal/util/util.go
@@ -1,10 +1,5 @@
 package util
 
-import (
-	"crypto/rand"
-	"math/big"
-)
-
 // OrString returns the first non-empty string.
 func OrString(o ...string) string {
 	for _, s := range o {
@@ -14,23 +9,4 @@ func OrString(o ...string) string {
 	}
 
 	return ""
-}
-
-// GenToken generates a random token of length based on the base58 alphabet without lower case chars.
-func GenToken(length int) string {
-	if length < 1 {
-		length = 6
-	}
-
-	// base58 alphabet to avoid character confusion without lower case chars
-	alphabet := "123456789ABCDEFGHJKLMNPQRSTUVWXYZ"
-	myReader := rand.Reader
-
-	token := ""
-	for i := 0; i < length; i++ {
-		letterAt, _ := rand.Int(myReader, big.NewInt(int64(len(alphabet))))
-		token += string(alphabet[letterAt.Uint64()])
-	}
-
-	return token
 }
